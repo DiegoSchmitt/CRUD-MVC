ccd49146e49d13d9b1290050fa86b2311572e264
Diego Schmitt <diego _asms@hotmail.com>
2022-10-11 20:08:37 -0300

    first commit

---
+++ src/views/partials/header.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>Meu CRUD em MVC</title>
+    <link rel="stylesheet" href="<?=$base;?>/assets/css/style.css">
+</head>
+<body>
+<header>
+    <h1>Titulo do site</h1>
+</header>
+<hr>
